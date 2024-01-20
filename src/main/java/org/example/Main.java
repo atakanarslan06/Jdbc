5af3d3f7843577a9b6d92fa14861ac48b2ea1ef4
Atakan Arslan <atak.arslan@hotmail.com>
2024-01-20 19:48:58 +0300

    MySql Connection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
 
+    static String userName="root";
+    static  String password="12345";
+    static  String dbUrl="jdbc=mysql://localhost:3306/Jdbc";
+
+    public void main(String[] args) throws  SQLException {
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(dbUrl,userName,password);
+            System.out.println("Bağlantı oluştu");
+        }catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+        finally {
+            {
+                connection.close();
+            }
+        }
     }
 }
